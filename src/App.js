35c244e920394b895285d9d18e96a7ce894d8af2
Qazi Wajahat Hussain <wajahathuss17@gmail.com>
2023-09-01 18:24:55 +0500

    Commit 1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import "./App.css"
+import Body from './components/Body'
+
+export default function App() {
+  return (
+    <div>
+      <>
+      <Navbar/>
+      <Body/>
+      <h6>Qazi Wajahat Hussain</h6>
+      </>
+    </div>
+  )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="navbar bg-dark">
+        <div className="container-fluid">
+          <div className="container text-center">
+            <h2 className="heading">Calculator</h2>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
